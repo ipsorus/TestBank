94e072d7093f1be796fda0a0643610cf8441b3d5
Ipsorus <ipsorus@inbox.ru>
2026-08-26 17:23:45 +0300

    first MR

---
+++ src/main/api/requests/create_account_requester.py
@@ -0,0 +1,17 @@
+import requests
+from requests.models import Response
+
+from src.main.api.models.create_account_response import CreateAccountResponse
+from src.main.api.requests.requester import Requester
+
+
+class CreateAccountRequester(Requester):
+    def post(self, model=None) -> CreateAccountResponse | Response:
+        url = f'{self.base_url}/account/create'
+        response = requests.post(
+            url=url,
+            headers=self.headers
+        )
+
+        self.response_spec(response)
+        return CreateAccountResponse(**response.json())
---
+++ src/main/api/tests/create_user_test.py
@@ -0,0 +1,37 @@
+from random import Random
+
+import pytest
+
+from src.main.api.generators.model_generator import RandomModelGenerator
+from src.main.api.models.create_user_request import CreateUserRequest
+
+
+@pytest.mark.api
+class TestCreateUser:
+
+    @pytest.mark.parametrize(
+        'create_user_request',
+        [RandomModelGenerator.generate(CreateUserRequest)])
+    def test_create_user_valid(self, api_manager, create_user_request):
+
+        response = api_manager.admin_steps.create_user(create_user_request)
+
+        assert create_user_request.username == response.username
+        assert create_user_request.role == response.role
+
+    @pytest.mark.parametrize('username, password', [
+        ('абв', 'Pas!sw0rd'),
+        ('ab', 'Pas!sw0rd'),
+        ('abv!', 'Pas!sw0rd'),
+        (f'Max{Random().randint(1, 100)}', 'Pas!sw0rд'),
+        (f'Max{Random().randint(1, 100)}', 'Pas!sw0'),
+        (f'Max{Random().randint(1, 100)}', 'pas!sw0rd'),
+        (f'Max{Random().randint(1, 100)}', 'PAS!SW0RD'),
+        (f'Max{Random().randint(1, 100)}', 'PASSSW0RD'),
+        (f'Max{Random().randint(1, 100)}', 'PAS!SWORD'),
+    ])
+    def test_create_user_invalid(self, username, password, api_manager):
+
+        create_user_request = CreateUserRequest(username=username, password=password, role='ROLE_USER')
+
+        api_manager.admin_steps.create_invalid_user(create_user_request)
